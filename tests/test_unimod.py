7f76477432f4698214cf646471d56fd34bad9410
fabianegli <fabian.egli@protonmail.ch>
2025-05-20 13:55:02 +0200

    instantiate UnimodDatabase only once in fixture

--- tests/test_unimod.py
+++ tests/test_unimod.py
@@ -1,14 +1,19 @@
+import pytest
+
 from sdrf_pipelines.openms.unimod import UnimodDatabase
 
 
-def test_search_mods_by_accession():
-    unimod = UnimodDatabase()
-    ptm = unimod.get_by_accession("UNIMOD:21")
+@pytest.fixture(scope="package")
+def unimod_db():
+    return UnimodDatabase()
+
+
+def test_search_mods_by_accession(unimod_db):
+    ptm = unimod_db.get_by_accession("UNIMOD:21")
     assert ptm.get_name() == "Phospho"
 
 
-def test_search_mods_by_keyword():
-    unimod = UnimodDatabase()
-    ptms = [ptm.to_str() for ptm in unimod.search_mods_by_keyword("Phospho")]
+def test_search_mods_by_keyword(unimod_db):
+    ptms = [ptm.to_str() for ptm in unimod_db.search_mods_by_keyword("Phospho")]
     assert "UNIMOD:21 Phospho 79.966331 H O(3) P" in ptms
     assert len(ptms) == 27
